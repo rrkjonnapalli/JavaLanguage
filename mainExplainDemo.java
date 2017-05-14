f3debc33b99b3b08c4009d2e68a1f24a5456c0df
rrkz <r.ravikiranjonnapalli@gmail.com>
2017-05-14 17:23:18 +0530

    Comment added here

--- mainExplainDemo.java
+++ mainExplainDemo.java
@@ -2,5 +2,6 @@
 public class mainExplainDemo {
 	public static void main(String args[]){
 		System.out.println(args.length);	
+		//main is static b'coz it should be called without creating object
 	}
 }
