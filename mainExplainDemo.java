eabf35ddccb179ae9b7050fd84fb93eb044c8e42
rrkz <r.ravikiranjonnapalli@gmail.com>
2017-05-14 17:54:46 +0530

    Update mainExplainDemo.java

--- mainExplainDemo.java
+++ mainExplainDemo.java
@@ -3,5 +3,6 @@ public class mainExplainDemo {
 	public static void main(String args[]){
 		System.out.println(args.length);	
 		//main is static b'coz it should be called without creating object
+		//args is command line arguments for execution.
 	}
 }
